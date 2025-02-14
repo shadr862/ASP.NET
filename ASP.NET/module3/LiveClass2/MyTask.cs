36fc9de1295b58d749e03592fd11994b7a76be9c
shadr862 <87410541+shadr862@users.noreply.github.com.>
2025-02-14 22:11:37 +0600

    module3 liveclass2

---
+++ ASP.NET/module3/LiveClass2/MyTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveClass2
+{
+    public class MyTask
+    {
+        public string TaskName { get; set; }
+        public MyTask()
+        {
+            TaskName = "Default";
+        }
+
+        public static int TaskNumber()
+        {
+            return 1;
+        }
+    }
+}
---
+++ ASP.NET/module3/LiveClass2/Stationary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveClass2
+{
+    public abstract class Stationary
+    {
+         public string StationaryName { get; set; }
+         public string GetStationaryName(string name)
+         {
+            this.StationaryName = name;
+            return this.StationaryName;
+         }
+
+        public abstract string GetStationaryInformation();
+
+
+    }
+}
