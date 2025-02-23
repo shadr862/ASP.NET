57867c50b79bb52478d3d46dc7c4e4a0787bf8c2
shadr862 <87410541+shadr862@users.noreply.github.com.>
2025-02-23 23:27:56 +0600

    livetest upade

---
+++ ASP.NET/Exam week-1/Livetest/livetest/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using livetest;
+
+Console.WriteLine("Hello, World!");
+// Value Type (int)
+
+int a = 10;
+int b = a; // Copy of 'a'
+b = 20;
+Console.WriteLine(a); // Output: 10
+
+// Reference Type (class)
+
+
+Person p1 = new Person();
+p1.Name = "John";
+Person p2 = p1; // Reference to 'p1'
+p2.Name = "Doe";
+Console.WriteLine(p1.Name); // Output: Doe
