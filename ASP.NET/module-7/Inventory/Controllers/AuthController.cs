eed0688c7060340fa31f91162c477bead09b7d81
shadr862 <87410541+shadr862@users.noreply.github.com.>
2025-03-11 14:27:53 +0600

    molde-7 update

---
+++ ASP.NET/module-7/Inventory/Controllers/AuthController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Models;
+
+namespace Inventory.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string txtUsername, string txtPassword)
+        {
+
+            /*
+            User obj = new User();
+            DataTable dt= obj.ValidateMemberAsDataTable(txtUsername, txtPassword);
+            if(dt.Rows.Count>0)
+            {
+                foreach(DataRow row in dt.Rows)
+                {
+                    int id = int.Parse(row["Id"].ToString());
+                    string name = row["Name"].ToString();
+                    string Pass = row["Password"].ToString();
+                    if(name==txtUsername && Pass==txtPassword)
+                    {
+                        return Redirect(Url.Action("Index", "Home"));
+                    }
+                    
+                }
+                ViewBag.UserName = txtUsername;
+                Session["UserName"] = txtUsername;
+                return Redirect(Url.Action("About", "Home"));
+            }
+            */
+
+            User obj = new User();
+            DataTable dt = obj.ValidateMemberAsDataTableBySp(txtUsername, txtPassword);
+            if (dt.Rows.Count > 0)
+            {
+                //foreach (DataRow row in dt.Rows)
+                //{
+                //    int id = int.Parse(row["Id"].ToString());
+                //    string name = row["Name"].ToString();
+                //    string Pass = row["Password"].ToString();
+                //    if (name == txtUsername && Pass == txtPassword)
+                //    {
+                //        return Redirect(Url.Action("Index", "Home"));
+                //    }
+
+                //}
+                ViewBag.UserName = txtUsername;
+                Session["UserName"] = txtUsername;
+                return Redirect(Url.Action("Index", "Dashboard"));
+            }
+            /*
+            User obj = new User();
+            List<User> list = obj.ValidateMemberAsList();
+            bool status = false;
+            foreach (User member in list)
+            {
+                if (member.Name == txtUsername && member.Password == txtPassword)
+                {
+                    status = true;
+                    break;
+                }
+            }
+            ViewBag.UserName = txtUsername;
+            Session["UserName"] = txtUsername;
+            if (status)
+            {
+                return Redirect(Url.Action("About", "Home"));
+            }
+            */
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("UserName");
+            return Redirect(@Url.Action("Login", "Auth"));
+        }
+    }
+}
---
+++ ASP.NET/module-7/Inventory/Models/User.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Optimization;
+
+namespace Inventory.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Role { get; set; }
+        public string Servicetype { get; set; }
+
+        public DataTable ValidateMemberAsDataTable(string UserName, string Password)
+        {
+            string Constring = ConfigurationManager.ConnectionStrings["EmployeeDBConnection"].ToString();
+
+            SqlConnection sqlConnection = new SqlConnection(Constring);
+            sqlConnection.Open();
+
+            string command = "select * from [dbo].[User] where Name='"+ UserName + "' and Password='"+Password+"'";
+            SqlCommand cmd = new SqlCommand(command, sqlConnection);
+            cmd.CommandTimeout = 0;
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Clear();
+
+            //Table Data
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dataTable);
+
+
+            cmd.Dispose();
+            sqlConnection.Close();
+            return dataTable;
+          
+        }
+        public DataTable ValidateMemberAsDataTableBySp(string UserName, string Password)
+        {
+            string Constring = ConfigurationManager.ConnectionStrings["EmployeeDBConnection"].ToString();
+
+            SqlConnection sqlConnection = new SqlConnection(Constring);
+            sqlConnection.Open();
+
+            
+            SqlCommand cmd = new SqlCommand("MyProcedure", sqlConnection);
+            cmd.CommandText = "MyProcedure";
+            cmd.Connection = sqlConnection;
+            cmd.CommandTimeout = 0;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add(new SqlParameter("@userName", UserName));
+            cmd.Parameters.Add(new SqlParameter("@Password", Password));
+            
+
+            //Table Data
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dataTable);
+
+
+            cmd.Dispose();
+            sqlConnection.Close();
+            return dataTable;
+
+        }
+
+        public List<User> ValidateMemberAsList()
+        {
+            string Appseting = ConfigurationManager.AppSettings["EmployeeDBConnectionAppSetting"].ToString();
+
+            SqlConnection sqlConnection = new SqlConnection(Appseting);
+            sqlConnection.Open();
+
+            string command = "select * from [dbo].[User]";
+            SqlCommand cmd = new SqlCommand(command, sqlConnection);
+            cmd.CommandTimeout = 0;
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Clear();
+
+            //List
+            List<User> users = new List<User>();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if(reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    User user = new User();
+                    user.Id = int.Parse(reader["Id"].ToString());
+                    user.Name = reader["Name"].ToString();
+                    user.Password = reader["Password"].ToString();
+                    user.Age = int.Parse(reader["Age"].ToString());
+                    user.Role = reader["Role"].ToString();
+                    user.Servicetype = reader["Servicetype"].ToString();
+                    users.Add(user);
+                }
+            }
+
+
+
+            cmd.Dispose();
+            sqlConnection.Close();
+
+            return users;
+        }
+
+    }
+
+    
+}
