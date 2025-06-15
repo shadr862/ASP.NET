37ef5f25184dd58f3b20799ce51b15c053464e16
riaz mehadi <shadr862@gmail.com>
2025-06-15 20:25:50 +0600

    assignment

---
+++ ASP.NET/Assignment/QuestionAndAnswer/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestuonAndAnswer.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
